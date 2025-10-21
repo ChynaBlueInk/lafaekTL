5563a455c5f6c83a0125be0c9a3b12c784fa5a27
ChynaBlueInk <chynablueink@gmail.com>
2025-10-21 09:09:19 +0900

    Update keystatic params code

--- app/api/keystatic/[[...params]]/route.ts
+++ app/api/keystatic/[[...params]]/route.ts
@@ -1,8 +1,12 @@
-// app/api/keystatic/[...params]/route.ts
+// app/api/keystatic/[[...params]]/route.ts
 export const runtime = 'nodejs';
 export const dynamic = 'force-dynamic';
 
-import { makeRouteHandler } from '@keystatic/next/route-handler';
+import { makeHandler } from '@keystatic/next/route-handler';
 import config from '../../../../keystatic.config';
 
-export const { GET, POST } = makeRouteHandler({ config });
+// Keystatic's built-in API handler (supports all /api/keystatic/* routes)
+export const { GET, POST } = makeHandler({
+  config,
+  secret: process.env.KEYSTATIC_SECRET!,
+});
