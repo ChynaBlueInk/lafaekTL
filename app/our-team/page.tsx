27b6165ed832a0b4a277bceeddef00852d44811f
Chyna Blue Ink <chynablueink@gmail.com>
2025-11-20 16:05:00 +0900

    Make our-team page dynamic

--- app/our-team/page.tsx
+++ app/our-team/page.tsx
@@ -1,10 +1,12 @@
 // app/our-team/page.tsx (SERVER, no Keystatic)
+export const dynamic="force-dynamic";
+
 import TeamClient from "./team-client";
 import {getTeamMembers} from "@/lib/content-team";
 import type {MemberFile} from "@/lib/content-team";
 
 export default async function OurTeamPage(){
-  const [membersEn, membersTet]:[MemberFile[],MemberFile[]]=await Promise.all([
+  const [membersEn,membersTet]:[MemberFile[],MemberFile[]]=await Promise.all([
     getTeamMembers("en"),
     getTeamMembers("tet")
   ]);
